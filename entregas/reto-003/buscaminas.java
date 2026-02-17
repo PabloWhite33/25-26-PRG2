ed241238502d91c5c58f2b60a74742faed82d7f7
pablo.white <pablo.white@alumnos.uneatlantico.es>
2026-02-17 13:17:59 +0100

    Paso 7: Mensaje final

--- entregas/reto-003/buscaminas.java
+++ entregas/reto-003/buscaminas.java
@@ -1,61 +1,59 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class buscaminas {
+public class Buscaminas {
 
     public static void imprimirTablero(char[][] tablero) {
+        System.out.println("================");
+        System.out.print("  ");
+        for (int j = 0; j < tablero[0].length; j++) {
+            System.out.print((j + 1) + " ");
+        }
+        System.out.println();
+
         for (int i = 0; i < tablero.length; i++) {
+            System.out.print((i + 1) + " ");
             for (int j = 0; j < tablero[i].length; j++) {
                 System.out.print(tablero[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println("================");
     }
 
     public static void main(String[] args) {
 
-        int explosiones = 0;
-        int descubiertas = 0;
-        int seguras = 35 - 5;
+        final int FILAS = 5;
+        final int COLUMNAS = 7;
+        final int MINAS = 5;
 
-        char[][] tablero = new char[5][7];
-        boolean[][] minas = new boolean[5][7];
-        Random rand = new Random();
+        char[][] tablero = new char[FILAS][COLUMNAS];
+        boolean[][] minas = new boolean[FILAS][COLUMNAS];
+
         Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
 
-        System.out.print("Ingrese X: ");
-        int x = sc.nextInt() - 1;
-
-        System.out.print("Ingrese Y: ");
-        int y = sc.nextInt() - 1;
+        for (int i = 0; i < FILAS; i++) {
+            for (int j = 0; j < COLUMNAS; j++) {
+                tablero[i][j] = '-';
+                minas[i][j] = false;
+            }
+        }
 
         int colocadas = 0;
-        while (colocadas < 5) {
-            int x = rand.nextInt(5);
-            int y = rand.nextInt(7);
-
+        while (colocadas < MINAS) {
+            int x = rand.nextInt(FILAS);
+            int y = rand.nextInt(COLUMNAS);
             if (!minas[x][y]) {
                 minas[x][y] = true;
                 colocadas++;
             }
         }
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 7; j++) {
-                tablero[i][j] = '-';
-            }
-        }
+        int explosiones = 0;
+        int descubiertas = 0;
+        int seguras = FILAS * COLUMNAS - MINAS;
 
-        imprimirTablero(tablero);
-        if (minas[x][y]) {
-            tablero[x][y] = '*';
-            System.out.println("Mina!");
-        } else {
-            tablero[x][y] = '.';
-            System.out.println("Libre!");
-        }
-
-        imprimirTablero(tablero);
         while (explosiones < 3 && descubiertas < seguras) {
 
             imprimirTablero(tablero);
@@ -66,13 +64,20 @@ public class buscaminas {
             System.out.print("Ingrese Y: ");
             int y = sc.nextInt() - 1;
 
-            if (tablero[x][y] != '-')
+            if (x < 0 || x >= FILAS || y < 0 || y >= COLUMNAS) {
+                System.out.println("Posición inválida");
                 continue;
+            }
+
+            if (tablero[x][y] != '-') {
+                System.out.println("Casilla ya abierta");
+                continue;
+            }
 
             if (minas[x][y]) {
                 tablero[x][y] = '*';
                 explosiones++;
-                System.out.println("Mina!");
+                System.out.println("Mina! Explosiones: " + explosiones);
             } else {
                 tablero[x][y] = '.';
                 descubiertas++;
@@ -80,5 +85,14 @@ public class buscaminas {
             }
         }
 
+        imprimirTablero(tablero);
+
+        if (explosiones >= 3) {
+            System.out.println("Perdiste!");
+        } else {
+            System.out.println("Ganaste!");
+        }
+
+        sc.close();
     }
 }
